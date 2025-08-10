e0a05f70242a0907c117b9a4c58202c3763cea97
bradygaster <bradyg@microsoft.com>
2025-08-10 11:43:09 -0700

    works with foundry local

--- OneMcpClient/Program.cs
+++ OneMcpClient/Program.cs
@@ -1,5 +1,29 @@
-﻿Console.WriteLine("What string would you like me to reverse?");
-Console.Write("> ");
-var input = Console.ReadLine();
+﻿using Microsoft.AI.Foundry.Local;
+using OpenAI;
+using System.ClientModel;
 
-Console.WriteLine("Connecting to server...");
+var alias = "phi-3.5-mini";
+
+var manager = await FoundryLocalManager.StartModelAsync(aliasOrModelId: alias);
+
+var model = await manager.GetModelInfoAsync(aliasOrModelId: alias);
+ApiKeyCredential key = new ApiKeyCredential(manager.ApiKey);
+OpenAIClient client = new OpenAIClient(key, new OpenAIClientOptions
+{
+    Endpoint = manager.Endpoint
+});
+
+var chatClient = client.GetChatClient(model?.ModelId);
+
+var completionUpdates = chatClient.CompleteChatStreaming("Why is the sky blue'");
+
+// todo: use the reverse mcp tool to reverse the string before printing it
+
+Console.Write($"[ASSISTANT]: ");
+foreach (var completionUpdate in completionUpdates)
+{
+    if (completionUpdate.ContentUpdate.Count > 0)
+    {
+        Console.Write(completionUpdate.ContentUpdate[0].Text);
+    }
+}
